8991ecabd9487fe74131d1c509bdcac85550f1e9
Roy Souvereyns <roy_souvereyns@hotmail.com>
2019-11-27 17:12:25 +0100

    rails yelp mvp starting css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create(name: 'Ribs', address: 'Leuven', category: 'belgian', phone_number: '0')
+Restaurant.create(name: 'McDo', address: 'Leuven', category: 'belgian', phone_number: '0')
+Restaurant.create(name: 'KFC', address: 'Leuven', category: 'belgian', phone_number: '0')
+Restaurant.create(name: 'Burger King', address: 'Leuven', category: 'belgian', phone_number: '0')
+Restaurant.create(name: 'Pizza', address: 'Leuven', category: 'italian', phone_number: '0')
